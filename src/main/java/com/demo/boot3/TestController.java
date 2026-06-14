dcc62e1418bd8402cbd2fae8dffc0b377913f507
liangzongji <liangzongji@gmail.com>
2026-06-14 16:18:06 +0800

    添加skywalking日志打印

--- src/main/java/com/demo/boot3/TestController.java
+++ src/main/java/com/demo/boot3/TestController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDateTime;
+
 @Slf4j
 @RestController
 public class TestController {
@@ -18,12 +20,12 @@ public class TestController {
     public String test(HttpServletRequest httpRequest) {
         log.info(httpRequest.toString());
         log.info("djiedjiejdiejidjei");
-        return "ok";
+        return "ok," + LocalDateTime.now().toString();
     }
 
     @GetMapping("test3")
     public String test3() {
-        String url = "http://localhost:8082/demo5/test";
+        String url = "http://localhost:8083/demo-boot3/test";
         String result = restTemplate.getForObject(url, String.class);
         log.info("调用外部接口返回结果: {}", result);
         return result;
